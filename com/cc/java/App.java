5a7b981ace3625256931169fb755237e9c50f6be
Peter Weber <peter-aalen@gmx.net>
2023-04-28 10:15:15 +0200

    Klausur.Try

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,14 +4,9 @@ public class App {
     
     public static void main(String[] args) {
 
-    // test --> Referenzvariable / RAM-Adresse des Objekts/Instanz (object/instance)
-    // Test --> Referenztyp (reference type)
-    // Object <--> Instanz
-
-    Test test  = new Test();
-    System.out.println("Referenzvariable: " + test);
-    test.showAddress();
-
+        KlausurProgrammierung obj = new KlausurProgrammierung();
+        obj.setItem("Klausur bestanden?");
+        System.out.println(obj.getItem());    
     }
 
 }
